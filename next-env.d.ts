e7d9505efb8f58f071b2df03cec81dc60f7f46c6
Shifana CT <85847088+shifanafin@users.noreply.github.com>
2026-09-25 14:38:53 +0400

    Update next-env.d.ts with new imports

--- next-env.d.ts
+++ next-env.d.ts
@@ -3,5 +3,6 @@
 import "./.next/types/routes.d.ts";
 import "./.next/types/root-params.d.ts";
 
+
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
